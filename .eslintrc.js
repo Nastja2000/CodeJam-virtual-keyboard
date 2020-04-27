128526205bdee0fb66dab2f496950ea71fa66efc
Anastasia Zizeko <nastjaznf@gmail.com>
2020-04-27 14:09:57 +0300

    refactor: solve eslint problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,23 +16,6 @@ module.exports = {
     },
     "rules": {
         "linebreak-style": "off",
-        "arrow-parens": "off",
-        "object-curly-newline": "off",
-        "no-mixed-operators": "off",
-        "arrow-body-style": "off",
-        "function-paren-newline": "off",
-        "space-before-function-paren": "off",
-
-        "func-names": "off",
-        "object-shorthand": "off",
-        "no-unused-vars": "off",
-        "no-param-reassign": "off",
-        "no-use-before-define": "off",
-
-        "max-len": ["error", 150, 2, { ignoreUrls: true, }],
-        "prettier/prettier": "off",
-        "no-alert": "error",
-        "no-console": "error",
-        "no-plusplus": "error"
+        "max-len": "off"
     }
 };
